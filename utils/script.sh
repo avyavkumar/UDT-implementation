e72b58ab2afa7bc1df52e835a57de25dcc29349d
avyavkumar <avyavkumar@gmail.com>
2016-04-17 15:18:27 +0530

    changes to util

--- utils/script.sh
+++ utils/script.sh
@@ -1,7 +1,7 @@
-g++ -c controlpacket.cpp -o controlpacket.o
-g++ -c datapacket.cpp -o datapacket.o
-g++ -c socket.cpp -o socket.o
-g++ -c packet.cpp -o packet.o
+g++ -c controlpacket.cpp -o controlpacket.o -std=c++11
+g++ -c datapacket.cpp -o datapacket.o -std=c++11
+g++ -c socket.cpp -o socket.o -std=c++11
+g++ -c packet.cpp -o packet.o -std=c++11
 g++ -c core.cpp -o core.o -std=c++11
 # g++ -c sendpacket.cpp -o sendpacket.o
 # g++ -c recvpacket.cpp -o recvpacket.o
