a93c1cf8b57e02733402dc147ba8de77745dd689
avyavkumar <avyavkumar@gmail.com>
2016-04-24 19:26:45 +0530

    changes

--- utils/script.sh
+++ utils/script.sh
@@ -3,7 +3,7 @@ g++ -c datapacket.cpp -o datapacket.o
 g++ -c socket.cpp -o socket.o
 g++ -c packet.cpp -o packet.o
 g++ -c core.cpp -o core.o -std=c++11
-g++ -c sendpacket.cpp -o sendpacket.o -std=c++11
-g++ -c recvpacket.cpp -o recvpacket.o -std=c++11
-g++ core.o packet.o controlpacket.o datapacket.o socket.o sendpacket.o -o sender
-g++ core.o packet.o controlpacket.o datapacket.o socket.o recvpacket.o -o receiver
+# g++ -c sendpacket.cpp -o sendpacket.o -std=c++11
+# g++ -c recvpacket.cpp -o recvpacket.o -std=c++11
+# g++ core.o packet.o controlpacket.o datapacket.o socket.o sendpacket.o -o sender
+# g++ core.o packet.o controlpacket.o datapacket.o socket.o recvpacket.o -o receiver
